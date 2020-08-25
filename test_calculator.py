0df99e59e912e679eeb2f9e94aea64d47d799166
Simonph <simonph@uio.no>
2020-08-25 15:56:20 +0200

    Parametriserte tester

--- test_calculator.py
+++ test_calculator.py
@@ -5,53 +5,62 @@ import pytest
 epsilon = 1e-10
 
 
-def test_add_exercise_1():
-    expected = 0.3
-    actual = calculator.add_exercise_1(0.2, 0.1)
+@pytest.mark.parametrize("x, y, ans", [(0.2, 0.1, 0.3), (1, 1, 2), (1/3, 1/3, 2/3), (3, 6, 9)])
+def test_add_exercise_1(x, y, ans):
+    expected = ans
+    actual = calculator.add_exercise_1(x, y)
     assert abs(actual - expected) < epsilon
 
 
-def test_add_strings_exercise_3():
-    expected = "hello world"
-    actual = calculator.add_exercise_1("hello ", "world")
+@pytest.mark.parametrize("x, y, ans", [("Hello ", "world", "Hello world"), ("1", "1", "11"), ("Hei", "Verden", "HeiVerden")])
+def test_add_strings_exercise_3(x, y, ans):
+    expected = ans
+    actual = calculator.add_exercise_1(x, y)
     assert actual == expected
 
 
-def test_factorial__exercise_4():
-    expected = 720
-    actual = calculator.factorial_exercise_4(6)
+@pytest.mark.parametrize("n, ans", [(1, 1), (2, 2), (6, 720)])
+def test_factorial__exercise_4(n, ans):
+    expected = ans
+    actual = calculator.factorial_exercise_4(n)
     assert abs(expected - actual) < epsilon
 
 
-def test_sin_exercise_4():
-    expected = 1
-    actual = calculator.sin_exercise_4(pi/2, 10)
+@pytest.mark.parametrize("x, ans", [(0, 0), (pi/2, 1), (pi, 0)])
+def test_sin_exercise_4(x, ans):
+    expected = ans
+    actual = calculator.sin_exercise_4(x, 10)
     assert abs(expected - actual) < epsilon
 
 
-def test_divide_exercise_4():
-    expected = 3.5
-    actual = calculator.divide_exercise_4(7, 2)
+@pytest.mark.parametrize("x, y, ans", [(1, 1, 1), (3, 2, 1.5), (7, 2, 3.5)])
+def test_divide_exercise_4(x, y, ans):
+    expected = ans
+    actual = calculator.divide_exercise_4(x, y)
     assert abs(expected - actual) < epsilon
 
 
-def test_multiply_exercise_4():
-    expected = 14
-    actual = calculator.multiply_exercise_4(7, 2)
+@pytest.mark.parametrize("x, y, ans", [(1, 1, 1), (3, 2, 6), (7, 2, 14)])
+def test_multiply_exercise_4(x, y, ans):
+    expected = ans
+    actual = calculator.multiply_exercise_4(x, y)
     assert abs(expected - actual) < epsilon
 
 
-def test_subtract_exercise_4():
-    expected = 5
-    actual = calculator.subtract_exercise_4(7, 2)
+@pytest.mark.parametrize("x, y, ans", [(1, 1, 0), (0.001, 0.0001, 0.0009), (7, 2, 5)])
+def test_subtract_exercise_4(x, y, ans):
+    expected = ans
+    actual = calculator.subtract_exercise_4(x, y)
     assert abs(expected - actual) < epsilon
 
 
-def test_add_raises_TypeError_exercise_5():
+@pytest.mark.parametrize("x, y", [(1, "hei verden"), (0.001, "0.001"), ("3", 5)])
+def test_add_raises_TypeError_exercise_5(x, y):
     with pytest.raises(TypeError):
-        calculator.add_exercise_1(4, "Hei verden")
+        calculator.add_exercise_1(x, y)
 
 
-def test_divide_raises_ZeroDivisionError_exercise_5():
+@pytest.mark.parametrize("x, y", [(1, 0), (0.001, 0), (6, 0)])
+def test_divide_raises_ZeroDivisionError_exercise_5(x, y):
     with pytest.raises(ZeroDivisionError):
-        calculator.divide_exercise_4(4, 0)
+        calculator.divide_exercise_4(x, y)
